08c315f6762969e3370a128f0abd5027a8ab809f
Anthony Bargnesi <abargnesi@selventa.com>
2013-11-21 13:36:08 -0500

    use conversion.rb module to provide ttl serializer

--- rdf_namespaces.rb
+++ rdf_namespaces.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 # vim: ts=2 sw=2:
-
+require './conversion.rb'
 require 'addressable/uri'
-require 'rdf'
-require 'rdf/ntriples'
 
 N = RDF::Vocabulary.new('http://www.selventa.com/bel/namespace/')
 GO = RDF::Vocabulary.new('http://www.geneontology.org/go#') # need GO:
@@ -26,10 +24,9 @@ Dir['namespaces/*beleq'].each do |path|
   end
 end
 
-def write_ns_stmts(hash, path, writer, out_file)
+def write_ns_stmts(hash, path, ttl)
   name = /namespaces\/([\w-]+).beleq/.match(path)[1]
-  out_file << writer.dump([RDF::Statement.new(
-    N[name], RDF.type, RDF::SKOS.ConceptScheme)])
+  ttl << RDF::Statement.new(N[name], RDF.type, RDF::SKOS.ConceptScheme)
 
   in_values = false
   File.foreach(path) do |l|
@@ -42,38 +39,28 @@ def write_ns_stmts(hash, path, writer, out_file)
     ident, uuid = l.split(%r{\|}).each { |v| v.strip! }
     res_uri = RDF::URI(Addressable::URI.encode(N["#{name}/#{ident}"].to_s))
 
-    out_file << writer.dump([RDF::Statement.new(
-      res_uri, RDF.type, RDF::SKOS.Concept)])
-    out_file << writer.dump([RDF::Statement.new(
-      res_uri, RDF::SKOS.inScheme, N[name])])
-    out_file << writer.dump([RDF::Statement.new(
-      res_uri, RDF::SKOS.prefLabel, ident)])
+    ttl << RDF::Statement.new(res_uri, RDF.type, RDF::SKOS.Concept)
+    ttl << RDF::Statement.new(res_uri, RDF::SKOS.inScheme, N[name])
+    ttl << RDF::Statement.new(res_uri, RDF::SKOS.prefLabel, ident)
 
     if name == 'go-biological-processes-ids' or name == 'go-cellular-component-ids'
-      out_file << writer.dump([RDF::Statement.new(
-        res_uri, RDF::RDFS.seeAlso, GO["GO:#{ident}"])])
+      ttl << RDF::Statement.new(res_uri, RDF::RDFS.seeAlso, GO["GO:#{ident}"])
     elsif name == 'swissprot-accession-numbers'
-      out_file << writer.dump([RDF::Statement.new(
-        res_uri, RDF::RDFS.seeAlso, UNIPROT[ident])])
+      ttl << RDF::Statement.new(res_uri, RDF::RDFS.seeAlso, UNIPROT[ident])
     elsif name == 'chebi-ids'
-      out_file << writer.dump([RDF::Statement.new(
-        res_uri, RDF::RDFS.seeAlso, CHEBI["CHEBI_#{ident}"])])
+      ttl << RDF::Statement.new(res_uri, RDF::RDFS.seeAlso, CHEBI["CHEBI_#{ident}"])
     elsif name == 'disease-ontology-ids'
-      out_file << writer.dump([RDF::Statement.new(
-        res_uri, RDF::RDFS.seeAlso, DO["DOID_#{ident}"])])
+      ttl << RDF::Statement.new(res_uri, RDF::RDFS.seeAlso, DO["DOID_#{ident}"])
     end
 
     hash[uuid].each do |e|
       match_uri = RDF::URI(Addressable::URI.encode(N[e].to_s))
-      out_file << writer.dump([RDF::Statement.new(
-        res_uri, RDF::SKOS.exactMatch, match_uri)])
+      ttl << RDF::Statement.new(res_uri, RDF::SKOS.exactMatch, match_uri)
     end
   end
 end
 
-writer = RDF::Writer.for(:ntriples)
-File.open("namespaces-skos.nt", "w") do |file|
-  Dir['namespaces/*beleq'].each do |path|
-    write_ns_stmts(hash, path, writer, file)
-  end
+ttl = BELRDF::Serializer.new(true)
+Dir['namespaces/*beleq'].each do |path|
+  write_ns_stmts(hash, path, ttl)
 end
